140e228309f7904328d60741e19f147b424c1d89
Aashish Chapain <ashishchapain86@gmail.com>
2023-11-03 14:22:25 +0545

    feat: completed user endpoint: GET, POST, PUT, DELETE

--- models/user.js
+++ models/user.js
@@ -11,26 +11,59 @@ const userSchema = new mongoose.Schema({
 
 const User = new mongoose.model('User', userSchema)
 
-function validate(user) {
+const complexityOptions = {
+    min: 8,
+    max: 255,
+    numeric: 1,
+    symbol: 1,
+};
+
+
+async function validatePost(user) {
     const schema = Joi.object({
         username: Joi.string().min(1).max(255).required(),
         email: Joi.string().min(3).max(255).email().required(),
         password: Joi.string().min(8).max(255).required()
     })
-    const complexityOptions = {
-        min: 8,
-        max: 255,
-        numeric: 1,
-        symbol: 1,
-    };
 
     const validation = schema.validate(user);
     const passwordValidation = passwordComplexity(complexityOptions).validate(user.password);
+    const userWithUsername = await User.findOne({ username: user.username });
+    const userWithEmail = await User.findOne({ email: user.email });
 
     if (validation.error) return validation
     if (passwordValidation.error) return passwordValidation
+    if (userWithUsername) return { error: { message: 'Username already exists' } };
+    if (userWithEmail) return { error: { message: 'Email already exists' } };
+    return true
+}
+
+async function validatePut(user) {
+    const schema = Joi.object({
+        username: Joi.string().min(1).max(255),
+        email: Joi.string().min(3).max(255).email(),
+        password: Joi.string().min(8).max(255)
+    })
+
+    const validation = schema.validate(user);
+    const passwordValidation = passwordComplexity(complexityOptions).validate(user.password);
+    if (validation.error) return validation
+    if (passwordValidation.error) return passwordValidation
+
+    if (user.username) {
+        const userWithUsername = await User.findOne({ username: user.username });
+        if (userWithUsername) return { error: { message: 'Username already exists' } };
+    }
+
+    if (user.email) {
+        const userWithEmail = await User.findOne({ email: user.email });
+        if (userWithEmail) return { error: { message: 'Email already exists' } };
+    }
+
     return true
 }
 
 
-module.exports = { User, validate }
+
+
+module.exports = { User, validatePost, validatePut }
--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,10 @@
-const { User, validate } = require('../models/user')
+const { User, validatePost, validatePut } = require('../models/user')
 const express = require('express')
 const _ = require('lodash')
 const router = express.Router()
 router.use(express.json())
 
-async function usernameOrEmailExists(username, email) {
-    const userWithUsername = await User.findOne({ username: username });
-    const userWithEmail = await User.findOne({ email: email });
-    if (userWithUsername) return { exists: true, message: 'Username already exists' };
-    if (userWithEmail) return { exists: true, message: 'Email already exists' };
-    return { exists: false, message: 'Username and email available' };
-}
+
 
 router.get('/', async (req, res) => {
     const user = await User.find({}, 'username email -_id').sort({ _id: -1 })
@@ -18,13 +12,33 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-    let result = validate(req.body)
+    const result = await validatePost(req.body)
     if (result.error) return res.status(400).json({ error: result.error.message })
-    result = await usernameOrEmailExists(req.body.username, req.body.email)
-    if (result.exists) return res.status(400).json({ error: result.message })
     const user = new User(_.pick(req.body, ['username', 'email', 'password']))
     const createdUser = await user.save()
     return res.send(_.pick(createdUser, ['username', 'email']))
+})
+
+router.get('/:username', async (req, res) => {
+    const user = await User.findOne({ username: req.params.username });
+    if (!user) return res.status(404).json({ error: "User Not Found" })
+    return res.send(_.pick(user, ['username', 'email']))
+})
+
+router.put('/:username', async (req, res) => {
+    const user = await User.findOne({ username: req.params.username });
+    if (!user) return res.status(404).json({ error: "User Not Found" })
+    const result = await validatePut(req.body)
+    if (result.error) return res.status(400).json({ error: result.error.message })
+    const updateUser = await User.updateOne({ username: req.params.username }, { $set: req.body })
+    res.send(updateUser)
+})
+
+router.delete('/:username', async (req, res) => {
+    const user = await User.findOne({ username: req.params.username });
+    if (!user) return res.status(404).json({ error: "User Not Found" })
+    const deleteUser = await User.findOneAndDelete({ username: req.params.username })
+    return res.send(_.pick(deleteUser, ['username', 'email']))
 })
 
 module.exports = router
